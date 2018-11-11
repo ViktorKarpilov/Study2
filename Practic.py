76efd76f97a089bd5d2737ce7122fb1eedacf6f1
ViktorKarpilov <wktorion@gmail.com>
2018-11-11 20:56:06 +0200

    Adding for and while

--- Practic.py
+++ Practic.py
@@ -1,8 +1,21 @@
 from my_library import validation
+
 numbers = validation.list_int_valid()
-def Sum(massive):
+def Recursivly_sum(massive):
     if(len(massive)>1):
-        return massive.pop() + Sum(massive)
+        return massive.pop() + Recursivly_sum(massive)
     else:
         return massive.pop()
-print(Sum(numbers))
+
+def For_sum(massive):
+    res = 0
+    for i in massive:
+        res +=i
+    return res
+
+def While_sum(massive):
+    res = 0
+    while(len(massive)>0):
+        res += massive.pop()
+    return res
+print(Recursivly_sum(numbers),For_sum(numbers),While_sum(numbers))
